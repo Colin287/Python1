620fa91bc37c71b2ab0ec64aae52e48909822ad0
Colin287 <ccampbell160@g.rwu.edu>
2021-09-16 14:03:19 -0400

    Assignment 2

---
+++ main.py
@@ -0,0 +1,22 @@
+#Programming Assignment 2
+# By: Colin Campbell
+# Date: 9/16/2021
+
+# Part 1
+name = input("Please enter your name: ")
+print(name)
+
+# Part 2
+user_num = int(input("Please enter a number: "))
+num_multi = user_num ** 2
+print(num_multi)
+
+# Part 3
+user_word = str(input("Please enter a single word: "))
+user_word_length = len(user_word)
+print("Your word has")
+print(user_word_length)
+print("letters")
+
+# Had to print it weird like that it kept throwing an error i didn't understand
+
